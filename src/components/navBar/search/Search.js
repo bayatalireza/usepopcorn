3c29fcd480c4c11f321d4041b9a8ec8883844a02
alireza <bayatalireza86@gmail.com>
2025-01-04 00:04:41 +0330

    `Refactored App.js and Search.js to use useKey hook instead of event listeners`

--- src/components/navBar/search/Search.js
+++ src/components/navBar/search/Search.js
@@ -1,26 +1,16 @@
-import { useEffect, useRef } from "react";
+import { useRef } from "react";
+import { useKey } from "../../../hooks/useKey";
 
 export default function Search({ query, setQuery }) {
-  // useEffect(()=>{
-  //   const el = document.querySelector('.search');
-
-  //   el.focus();
-  // }, [])
 
 
   const inputEl = useRef(null);
-  useEffect(() => {
+  useKey("Enter", function () {
+    if (document.activeElement === inputEl.current) return;
 
-    
-    function callBack(e){
-      if(document.activeElement === inputEl.current) return;
-      if(e.code === "Enter"){}
-      inputEl.current.focus();
-      setQuery("");
-    }
-    document.addEventListener("keydown", callBack);
-    return ()=> document.removeEventListener("keydown", callBack);
-  }, [setQuery]);
+    inputEl.current.focus();
+    setQuery("");
+  });
 
   return (
     <>
